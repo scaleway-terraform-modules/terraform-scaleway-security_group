5775648701f391e28a0cc85902827c476a9666c2
Benoit Garcia <benoit.garcia@protonmail.com>
2023-01-28 14:33:41 +0100

    chore(terraform): Reformat outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "group_id" {
   description = "ID of the security group."
-  sensitive = false
-  value = scaleway_instance_security_group.this.id
+  sensitive   = false
+  value       = scaleway_instance_security_group.this.id
 }
