dfdf3514ffcf50dead20c7ac4a0b347ab7465c89
Benoit Garcia <benoit.garcia@protonmail.com>
2023-01-28 14:34:11 +0100

    chore(terrafom): Ensure all input variables are documented and re-order their attributes.

--- variables.tf
+++ variables.tf
@@ -1,52 +1,66 @@
 variable "name" {
-  type    = string
-  default = "git-servers"
+  description = "Name of the security group."
+  type        = string
+  default     = null
 }
 
+variable "description" {
+  description = "Description of the security group."
+  type        = string
+  default     = null
+}
 variable "stateful" {
-  type    = bool
-  default = true
+  description = "Boolean to specify whether the security group should be stateful or not."
+  type        = bool
+  default     = true
 }
 
 variable "inbound_default_policy" {
-  type    = string
-  default = "drop"
+  description = "Default policy on incoming traffic. Possible values are: accept or drop."
+  type        = string
+  default     = "drop"
+
+  validation {
+    condition     = contains(["accept", "drop"], var.inbound_default_policy)
+    error_message = "The inbound_default_policy value must be one of 'accept' or 'drop'."
+  }
 }
 
 variable "outbound_default_policy" {
-  type    = string
-  default = "drop"
-}
+  description = "Default policy on outgoing traffic. Possible values are: accept or drop."
+  type        = string
+  default     = "drop"
 
-variable "tags" {
-  type        = list(any)
-  description = "Tags associated with the security group and its rules."
-  default     = []
-}
-
-variable "enable_default_security" {
-  type    = bool
-  default = true
+  validation {
+    condition     = contains(["accept", "drop"], var.outbound_default_policy)
+    error_message = "The outbound_default_policy value must be one of 'accept' or 'drop'."
+  }
 }
 
 variable "inbound_rules" {
+  description = "List of inbound rule to add to the security group."
   type = list(object({
-    action   = string
-    protocol = string
-    port     = optional(number)
-    ip_range = optional(string)
+    action     = string
+    protocol   = string
+    port       = optional(number)
+    port_range = optional(string)
+    ip         = string
+    ip_range   = optional(string)
   }))
   default = [
     {
-      action   = null
-      protocol = null
-      port     = null
-      ip_range = null
+      action     = null
+      protocol   = null
+      port       = null
+      port_range = null
+      ip         = null
+      ip_range   = null
     }
   ]
 }
 
 variable "outbound_rules" {
+  description = "List of outbound rule to add to the security group."
   type = list(object({
     action   = string
     protocol = string
@@ -61,4 +75,16 @@ variable "outbound_rules" {
       ip_range = null
     }
   ]
+}
+
+variable "enable_default_security" {
+  description = "Whether to block SMTP on IPv4/IPv6 (Port 25, 465, 587). Set to false will unblock SMTP if your account is authorized to. If your organization is not yet authorized to send SMTP traffic, open a support ticket."
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Tags associated with the security group and its rules."
+  type        = list(any)
+  default     = []
 }
